f820577ec41d28e3af2f3bd7ed3f1ce92f1999a8
Janice Lichtman <jalichtman@gmail.com>
2017-02-23 12:31:32 -0800

    Basic Stack and Queue implemented

--- lib/Queue.rb
+++ lib/Queue.rb
@@ -1,22 +1,29 @@
 class Queue
   def initialize
+      @store = Array.new
   end
 
   def enqueue(element)
+    @store.push(element)
   end
 
   def dequeue
+    return @store.shift
   end
 
   def front
+    return @store.first
   end
 
   def size
+    return @store.length
   end
 
   def empty?
+    return @store.length == 0
   end
 
   def to_s
+    return @store.to_s
   end
 end
--- lib/Stack.rb
+++ lib/Stack.rb
@@ -4,18 +4,27 @@ class Stack
   end
 
   def push(element)
+    @store.push(element)
+    #OR @store << element
+    #OR Can use @store.unshift(element), but new elements will be coming onto the beginning instead of the end of the array.
   end
 
   def pop
+     return @store.pop
   end
 
   def top
+    return @store.last
   end
 
   def size
+    return @store.length
   end
 
   def empty?
+    return @store.length == 0
+    #OR return size == 0
+    #OR return @store.empty?
   end
 
   def to_s
