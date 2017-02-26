dc68a857781e82e9d79bf962e3f84efc8995de7e
Janice Lichtman <jalichtman@gmail.com>
2017-02-26 00:31:16 -0800

    Job Simulation complete

--- lib/job-simulation.rb
+++ lib/job-simulation.rb
@@ -1,31 +1,50 @@
-require './Stack.rb'
-require './Queue.rb'
+require './lib/Stack.rb'
+require './lib/Queue.rb'
 
 class JobSimulation
   attr_reader :workers, :waiting, :roll
 
   def initialize (jobs_available, job_seekers)
+    @waiting = Queue.new
+    job_seekers.times {|n|
+      @waiting.enqueue("Worker ##{n+1}")
+    }
 
+    @workers = Stack.new
+    jobs_available.times {
+      @workers.push(@waiting.dequeue)
+    }
+
+    @roll = 0
   end
+
 
   def cycle
-
+    @roll = rand(1..6) #assuming a six-sided die
+    @roll.times {
+      @workers.pop
+    }
+    @roll.times {
+      @workers.push(@waiting.dequeue)
+    }
   end
+
 end
 
-## Allows us to run our code and see what's happening:
-sim = JobSimulation.new(6,10)
-puts "------------------------------"
-puts "Before the simulation starts"
-puts "Employed: #{sim.workers}"
-puts "Waitlist: #{sim.waiting}"
-puts "------------------------------"
-print "<enter to cycle>\n"
 
-count = 0
-until gets.chomp != ""
-  puts "-------Cycle #{count+=1}-------"
-  sim.cycle
-  puts "Employed: #{sim.workers}"
-  puts "Waitlist: #{sim.waiting}"
-end
+# ## Allows us to run our code and see what's happening:
+# sim = JobSimulation.new(6,10)
+# puts "------------------------------"
+# puts "Before the simulation starts"
+# puts "Employed: #{sim.workers}"
+# puts "Waitlist: #{sim.waiting}"
+# puts "------------------------------"
+# print "<enter to cycle>\n"
+#
+# count = 0
+# until gets.chomp != ""
+#   puts "-------Cycle #{count+=1}-------"
+#   sim.cycle
+#   puts "Employed: #{sim.workers}"
+#   puts "Waitlist: #{sim.waiting}"
+# end
--- specs/test-job-simulation.rb
+++ specs/test-job-simulation.rb
@@ -5,4 +5,37 @@ Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 describe "Test Terrible Company" do
 
+  describe "job simulation implementation" do
+    it "creates a JobSimulation" do
+      q = JobSimulation.new(6,10)
+      q.class.must_equal JobSimulation
+    end
+
+    it "begins with a number of workers equal to the number of jobs available" do
+      q =JobSimulation.new(6,10)
+      q.workers.size.must_equal 6
+    end
+
+    it "begins with waiting list equal to the number of available workers left after jobs available are filled" do
+      q =JobSimulation.new(6,10)
+      q.waiting.size.must_equal (10-6)
+    end
+  end
+
+  describe "Cycle" do
+
+    describe "roll" do
+      it "generates an integer from 1 to 6" do
+        q =JobSimulation.new(6,10)
+        q.cycle
+        (1..6).must_include(q.roll)
+      end
+    end
+
+    it "removes a number of people off the worker list, and then moves that same number of people from the waitlist to the worker list" do
+      q =JobSimulation.new(1,10)
+      q.cycle
+      q.waiting.size.must_equal (10 - 1 - q.workers.size)
+    end
+  end
 end
